d1dc3eac578c4151695c74bc416c0832967d1371
Saad Malik <36926771+maliksh7@users.noreply.github.com>
2021-10-14 20:56:06 +0500

    .

---
+++ layout/MainFooter.vue
@@ -0,0 +1,48 @@
+<template>
+  <footer
+    class="footer"
+    :class="{ [`footer-${type}`]: type }"
+    :data-background-color="backgroundColor"
+  >
+    <div class="container">
+      <nav>
+        <ul>
+          <li>
+            <a href="">
+              About Us
+            </a>
+          </li>
+          <li>
+            <a href="">
+              Blog
+            </a>
+          </li>
+          <li>
+            <a href="">
+              Licenses
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div class="copyright">
+        &copy; {{ year }}, made with <md-icon>favorite</md-icon> by
+        <a href="" target="_blank">Mfused Army</a>
+        for a better web.
+      </div>
+    </div>
+  </footer>
+</template>
+<script>
+export default {
+  props: {
+    backgroundColor: String,
+    type: String,
+  },
+  data() {
+    return {
+      year: new Date().getFullYear(),
+    };
+  },
+};
+</script>
+<style></style>
